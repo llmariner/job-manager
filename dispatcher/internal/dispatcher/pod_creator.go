8d8dd96fb7d797e6c10a2f09bb23a8c4fcd58c5f
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-05 21:11:28 -0700

    dispatcher: Make fixes to real jobsg (#27)

--- dispatcher/internal/dispatcher/pod_creator.go
+++ dispatcher/internal/dispatcher/pod_creator.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -20,12 +21,13 @@ func NewPodCreator(
 	k8sClient kubernetes.Interface,
 	namespace string,
 	modelStoreConfig *config.ModelStoreConfig,
+	useFakeJob bool,
 ) *PodCreator {
 	return &PodCreator{
 		k8sClient:        k8sClient,
 		namespace:        namespace,
 		modelStoreConfig: modelStoreConfig,
-		fakeJob:          true,
+		useFakeJob:       useFakeJob,
 	}
 }
 
@@ -35,84 +37,20 @@ type PodCreator struct {
 	// TODO(kenji): Be able to specify the namespace per tenant.
 	namespace        string
 	modelStoreConfig *config.ModelStoreConfig
-	fakeJob          bool
+	useFakeJob       bool
 }
 
 func (p *PodCreator) createPod(ctx context.Context, job *store.Job) error {
 	// TODO(kenji): Create a real fine-tuning job. See https://github.com/llm-operator/job-manager/tree/main/build/experiments/fine-tuning.
 
+	log.Printf("Creating a pod for job %s\n", job.JobID)
 	podName := fmt.Sprintf("job-%s", job.JobID)
-
-	var image, cmd string
-	if p.fakeJob {
-		image = "llm-operator/experiments-fake-job:latest"
-		cmd = "mkdir /models/adapter; cp ./ggml-adapter-model.bin /models/adapter/ggml-adapter-model.bin"
-	} else {
-		image = "llm-operator/experiments-fine-tuning:latest"
-		cmd = `
-mkdir /models/adapter ;
-accelerate launch \
-  --config_file=./single_gpu.yaml \
-  --num_processes=1 \
-  ./sft.py \
-  --model_name=google/gemma-2b \
-  --dataset_name=OpenAssistant/oasst_top1_2023-08-25 \
-  --per_device_train_batch_size=2 \
-  --gradient_accumulation_steps=1 \
-  --max_steps=10 \
-  --learning_rate=2e-4 \
-  --save_steps=20_000 \
-  --use_peft \
-  --lora_r=16 \
-  --lora_alpha=32 \
-  --lora_target_modules q_proj k_proj v_proj o_proj \
-  --load_in_4bit \
-  --output_dir=./output &&
-  python ./convert-lora-to-ggml.py /models/adapter/
-`
-	}
-
-	var volumeMounts []corev1.VolumeMount
-	var volumes []corev1.Volume
-	mountPath := "/models"
-	if ms := p.modelStoreConfig; ms.Enable {
-		const vname = "model-store"
-		volumeMounts = append(volumeMounts, corev1.VolumeMount{
-			Name:      vname,
-			MountPath: mountPath,
-		})
-
-		volumes = append(volumes, corev1.Volume{
-			Name: vname,
-			VolumeSource: corev1.VolumeSource{
-				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-					ClaimName: ms.PVClaimName,
-				},
-			},
-		})
-	}
-
 	pod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: podName,
 		},
-		Spec: corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:            "main",
-					Image:           image,
-					ImagePullPolicy: "Never",
-					Command:         []string{"/bin/bash", "-c", cmd},
-					VolumeMounts:    volumeMounts,
-					// TODO(kenji): Request GPU
-				},
-			},
-			Volumes:       volumes,
-			RestartPolicy: corev1.RestartPolicyOnFailure,
-		},
+		Spec: p.podSpec(),
 	}
-
-	log.Printf("Creating a pod for job %s\n", job.JobID)
 	if _, err := p.k8sClient.CoreV1().Pods(p.namespace).Create(ctx, pod, metav1.CreateOptions{}); err != nil {
 		if apierrors.IsAlreadyExists(err) {
 			// TODO(kenji): Revisit this error handling.
@@ -150,4 +88,77 @@ accelerate launch \
 
 	log.Printf("Pod finished running\n")
 	return nil
+}
+
+func (p *PodCreator) podSpec() corev1.PodSpec {
+	var image, cmd string
+	var res corev1.ResourceRequirements
+	if p.useFakeJob {
+		image = "llm-operator/experiments-fake-job:latest"
+		cmd = "mkdir /models/adapter; cp ./ggml-adapter-model.bin /models/adapter/ggml-adapter-model.bin"
+	} else {
+		image = "llm-operator/experiments-fine-tuning:latest"
+		cmd = `
+mkdir /models/adapter;
+accelerate launch \
+  --config_file=./single_gpu.yaml \
+  --num_processes=1 \
+  ./sft.py \
+  --model_name=google/gemma-2b \
+  --dataset_name=OpenAssistant/oasst_top1_2023-08-25 \
+  --per_device_train_batch_size=2 \
+  --gradient_accumulation_steps=1 \
+  --max_steps=100 \
+  --learning_rate=2e-4 \
+  --save_steps=20_000 \
+  --use_peft \
+  --lora_r=16 \
+  --lora_alpha=32 \
+  --lora_target_modules q_proj k_proj v_proj o_proj \
+  --load_in_4bit \
+  --output_dir=./output &&
+python ./convert-lora-to-ggml.py ./output &&
+cp ./output/ggml-adapter-model.bin /models/adapter/
+`
+		res = corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				"nvidia.com/gpu": *resource.NewQuantity(1, resource.DecimalSI),
+			},
+		}
+	}
+
+	var volumeMounts []corev1.VolumeMount
+	var volumes []corev1.Volume
+	mountPath := "/models"
+	if ms := p.modelStoreConfig; ms.Enable {
+		const vname = "model-store"
+		volumeMounts = append(volumeMounts, corev1.VolumeMount{
+			Name:      vname,
+			MountPath: mountPath,
+		})
+
+		volumes = append(volumes, corev1.Volume{
+			Name: vname,
+			VolumeSource: corev1.VolumeSource{
+				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+					ClaimName: ms.PVClaimName,
+				},
+			},
+		})
+	}
+
+	return corev1.PodSpec{
+		Containers: []corev1.Container{
+			{
+				Name:            "main",
+				Image:           image,
+				ImagePullPolicy: "Never",
+				Command:         []string{"/bin/bash", "-c", cmd},
+				Resources:       res,
+				VolumeMounts:    volumeMounts,
+			},
+		},
+		Volumes:       volumes,
+		RestartPolicy: corev1.RestartPolicyOnFailure,
+	}
 }
