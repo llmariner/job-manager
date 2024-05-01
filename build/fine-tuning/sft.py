6522c95d9d06dfd7d86db333fb9152d83ed44419
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-05-01 13:53:08 -0700

    Update sft.py

--- build/fine-tuning/sft.py
+++ build/fine-tuning/sft.py
@@ -2,21 +2,6 @@
 #
 # The code is slightly modified to remove the logic for using a rich progress bar.
 
-# flake8: noqa
-# Copyright 2023 The HuggingFace Inc. team. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import logging
 import os
 import argparse
@@ -58,10 +43,6 @@ if __name__ == "__main__":
     )
 
     model_kwargs = dict(
-        # The specific model version to use (can be a branch name, tag name or commit id)."
-        # revision="main",
-        # Trust remote code when loading a model.
-        # trust_remote_code=False,
         # Which attention implementation to use; you can run --attn_implementation=flash_attention_2,
         # in which case you must install this manually by running `pip install flash-attn --no-build-isolation`
         attn_implementation=None,
