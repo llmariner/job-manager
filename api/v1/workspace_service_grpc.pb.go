42638c7ee4d3853c8becd1186c096352a80c86d4
Aya (Igarashi) Ozawa <Ladiclexxx@gmail.com>
2024-06-06 10:45:45 +0900

    Add WorkspaceWorkerService (#179)

--- api/v1/workspace_service_grpc.pb.go
+++ api/v1/workspace_service_grpc.pb.go
@@ -292,3 +292,131 @@ var WorkspaceService_ServiceDesc = grpc.ServiceDesc{
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "api/v1/workspace_service.proto",
 }
+
+const (
+	WorkspaceWorkerService_ListQueuedInternalNotebooks_FullMethodName = "/llmoperator.workspace.server.v1.WorkspaceWorkerService/ListQueuedInternalNotebooks"
+	WorkspaceWorkerService_UpdateNotebookState_FullMethodName         = "/llmoperator.workspace.server.v1.WorkspaceWorkerService/UpdateNotebookState"
+)
+
+// WorkspaceWorkerServiceClient is the client API for WorkspaceWorkerService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
+type WorkspaceWorkerServiceClient interface {
+	ListQueuedInternalNotebooks(ctx context.Context, in *ListQueuedInternalNotebooksRequest, opts ...grpc.CallOption) (*ListQueuedInternalNotebooksResponse, error)
+	UpdateNotebookState(ctx context.Context, in *UpdateNotebookStateRequest, opts ...grpc.CallOption) (*UpdateNotebookStateResponse, error)
+}
+
+type workspaceWorkerServiceClient struct {
+	cc grpc.ClientConnInterface
+}
+
+func NewWorkspaceWorkerServiceClient(cc grpc.ClientConnInterface) WorkspaceWorkerServiceClient {
+	return &workspaceWorkerServiceClient{cc}
+}
+
+func (c *workspaceWorkerServiceClient) ListQueuedInternalNotebooks(ctx context.Context, in *ListQueuedInternalNotebooksRequest, opts ...grpc.CallOption) (*ListQueuedInternalNotebooksResponse, error) {
+	out := new(ListQueuedInternalNotebooksResponse)
+	err := c.cc.Invoke(ctx, WorkspaceWorkerService_ListQueuedInternalNotebooks_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *workspaceWorkerServiceClient) UpdateNotebookState(ctx context.Context, in *UpdateNotebookStateRequest, opts ...grpc.CallOption) (*UpdateNotebookStateResponse, error) {
+	out := new(UpdateNotebookStateResponse)
+	err := c.cc.Invoke(ctx, WorkspaceWorkerService_UpdateNotebookState_FullMethodName, in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// WorkspaceWorkerServiceServer is the server API for WorkspaceWorkerService service.
+// All implementations must embed UnimplementedWorkspaceWorkerServiceServer
+// for forward compatibility
+type WorkspaceWorkerServiceServer interface {
+	ListQueuedInternalNotebooks(context.Context, *ListQueuedInternalNotebooksRequest) (*ListQueuedInternalNotebooksResponse, error)
+	UpdateNotebookState(context.Context, *UpdateNotebookStateRequest) (*UpdateNotebookStateResponse, error)
+	mustEmbedUnimplementedWorkspaceWorkerServiceServer()
+}
+
+// UnimplementedWorkspaceWorkerServiceServer must be embedded to have forward compatible implementations.
+type UnimplementedWorkspaceWorkerServiceServer struct {
+}
+
+func (UnimplementedWorkspaceWorkerServiceServer) ListQueuedInternalNotebooks(context.Context, *ListQueuedInternalNotebooksRequest) (*ListQueuedInternalNotebooksResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListQueuedInternalNotebooks not implemented")
+}
+func (UnimplementedWorkspaceWorkerServiceServer) UpdateNotebookState(context.Context, *UpdateNotebookStateRequest) (*UpdateNotebookStateResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateNotebookState not implemented")
+}
+func (UnimplementedWorkspaceWorkerServiceServer) mustEmbedUnimplementedWorkspaceWorkerServiceServer() {
+}
+
+// UnsafeWorkspaceWorkerServiceServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to WorkspaceWorkerServiceServer will
+// result in compilation errors.
+type UnsafeWorkspaceWorkerServiceServer interface {
+	mustEmbedUnimplementedWorkspaceWorkerServiceServer()
+}
+
+func RegisterWorkspaceWorkerServiceServer(s grpc.ServiceRegistrar, srv WorkspaceWorkerServiceServer) {
+	s.RegisterService(&WorkspaceWorkerService_ServiceDesc, srv)
+}
+
+func _WorkspaceWorkerService_ListQueuedInternalNotebooks_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListQueuedInternalNotebooksRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WorkspaceWorkerServiceServer).ListQueuedInternalNotebooks(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: WorkspaceWorkerService_ListQueuedInternalNotebooks_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WorkspaceWorkerServiceServer).ListQueuedInternalNotebooks(ctx, req.(*ListQueuedInternalNotebooksRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _WorkspaceWorkerService_UpdateNotebookState_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateNotebookStateRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(WorkspaceWorkerServiceServer).UpdateNotebookState(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: WorkspaceWorkerService_UpdateNotebookState_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(WorkspaceWorkerServiceServer).UpdateNotebookState(ctx, req.(*UpdateNotebookStateRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+// WorkspaceWorkerService_ServiceDesc is the grpc.ServiceDesc for WorkspaceWorkerService service.
+// It's only intended for direct use with grpc.RegisterService,
+// and not to be introspected or modified (even as a copy)
+var WorkspaceWorkerService_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "llmoperator.workspace.server.v1.WorkspaceWorkerService",
+	HandlerType: (*WorkspaceWorkerServiceServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "ListQueuedInternalNotebooks",
+			Handler:    _WorkspaceWorkerService_ListQueuedInternalNotebooks_Handler,
+		},
+		{
+			MethodName: "UpdateNotebookState",
+			Handler:    _WorkspaceWorkerService_UpdateNotebookState_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "api/v1/workspace_service.proto",
+}
