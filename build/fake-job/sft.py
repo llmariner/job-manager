1e1aeeede1526ccbfc9966502d21a1907ffbc753
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-10-08 23:25:24 -0700

    fix

--- build/fake-job/sft.py
+++ build/fake-job/sft.py
@@ -3,6 +3,6 @@ import os
 # Move files in the current directory to the "output" directory.
 files = os.listdir()
 for fn in files:
-    if fn == "output" || fn.endswith(".py"):
+    if fn == "output" or fn.endswith(".py"):
         continue
     os.rename(fn, f"output/{fn}")
