ad5ebf797880d02047085416eeb9b6ef43c6c255
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-10-08 23:59:35 -0700

    Set adapter when registring a model

--- dispatcher/internal/dispatcher/pre_processor.go
+++ dispatcher/internal/dispatcher/pre_processor.go
@@ -7,9 +7,9 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	fv1 "github.com/llmariner/file-manager/api/v1"
 	v1 "github.com/llmariner/job-manager/api/v1"
 	is3 "github.com/llmariner/job-manager/dispatcher/internal/s3"
-	fv1 "github.com/llmariner/file-manager/api/v1"
 	mv1 "github.com/llmariner/model-manager/api/v1"
 	"google.golang.org/grpc"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -115,8 +115,10 @@ func (p *PreProcessor) Process(ctx context.Context, job *v1.InternalJob) (*PrePr
 	}
 
 	rresp, err := p.modelClient.RegisterModel(ctx, &mv1.RegisterModelRequest{
-		BaseModel:      job.Job.Model,
-		Suffix:         job.Suffix,
+		BaseModel: job.Job.Model,
+		Suffix:    job.Suffix,
+		// TODO(kenji): Revist.
+		Adapter:        mv1.AdapterType_ADAPTER_TYPE_LORA,
 		OrganizationId: job.Job.OrganizationId,
 		ProjectId:      job.Job.ProjectId,
 	})
