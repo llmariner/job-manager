0779ab41b71c24259579cd7199e0b3859b66dea6
Kenji Kaneda <kenji@Kenjis-MacBook-Air.local>
2024-12-25 19:40:01 -0800

    feat(api): add an API for sending the cluster status
    Server needs to know if a job can run on a cluster to make a
    scheduling decision.
    
    The actual scheduling algorithm is TBD, but this is adding a
    framework for that.

---
+++ ts/api/v1/job_manager_server_worker.pb.ts
@@ -0,0 +1,28 @@
+/* eslint-disable */
+// @ts-nocheck
+/*
+* This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
+*/
+
+import * as fm from "../../fetch.pb"
+export type ProvisionableResource = {
+  instanceFamily?: string
+  instanceType?: string
+}
+
+export type ClusterStatus = {
+  provisionableResources?: ProvisionableResource[]
+}
+
+export type UpdateClusterStatusRequest = {
+  status?: ClusterStatus
+}
+
+export type UpdateClusterStatusResponse = {
+}
+
+export class JobWorkerService {
+  static UpdateClusterStatus(req: UpdateClusterStatusRequest, initReq?: fm.InitReq): Promise<UpdateClusterStatusResponse> {
+    return fm.fetchReq<UpdateClusterStatusRequest, UpdateClusterStatusResponse>(`/llmariner.jobs.server.v1.JobWorkerService/UpdateClusterStatus`, {...initReq, method: "POST", body: JSON.stringify(req)})
+  }
+}
