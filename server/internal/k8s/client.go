db0e48951f3c47e497020835751d65fac458596b
Aya (Igarashi) Ozawa <Ladiclexxx@gmail.com>
2025-01-17 16:36:26 +0900

    feat(server): implement syncer service APIs (#336)

--- server/internal/k8s/client.go
+++ server/internal/k8s/client.go
@@ -5,7 +5,11 @@ import (
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	corev1apply "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
@@ -14,7 +18,8 @@ const fieldManager = "job-manager-server"
 
 // ClientFactory is a factory to create a Client.
 type ClientFactory interface {
-	NewClient(clusterID string, token string) (Client, error)
+	NewClient(clusterID, token string) (Client, error)
+	NewDynamicClient(clusterID, token string) (DynamicClient, error)
 }
 
 // NewClientFactory creates a new ClientFactory.
@@ -27,15 +32,28 @@ type defaultClientFactory struct {
 }
 
 // NewK8sClient creates a new Client.
-func (f *defaultClientFactory) NewClient(clusterID string, token string) (Client, error) {
-	client, err := kubernetes.NewForConfig(&rest.Config{
-		Host:        fmt.Sprintf("%s/sessions/%s", f.endpoint, clusterID),
-		BearerToken: token,
-	})
+func (f *defaultClientFactory) NewClient(clusterID, token string) (Client, error) {
+	client, err := kubernetes.NewForConfig(f.getRestConfig(clusterID, token))
 	if err != nil {
 		return nil, err
 	}
 	return &defaultClient{client: client}, nil
+}
+
+// NewDynamicK8sClient creates a new dynamic Client.
+func (f *defaultClientFactory) NewDynamicClient(clusterID, token string) (DynamicClient, error) {
+	client, err := dynamic.NewForConfig(f.getRestConfig(clusterID, token))
+	if err != nil {
+		return nil, err
+	}
+	return NewDefaultDynamicClient(client), nil
+}
+
+func (f *defaultClientFactory) getRestConfig(clusterID, token string) *rest.Config {
+	return &rest.Config{
+		Host:        fmt.Sprintf("%s/sessions/%s", f.endpoint, clusterID),
+		BearerToken: token,
+	}
 }
 
 // Client is a client to mange worker Kubernetes resources.
@@ -62,4 +80,37 @@ func (c *defaultClient) CreateConfigMap(ctx context.Context, name, namespace str
 	conf := corev1apply.ConfigMap(name, namespace).WithBinaryData(data)
 	_, err := c.client.CoreV1().ConfigMaps(namespace).Apply(ctx, conf, opts)
 	return err
+}
+
+// DynamicClient is a dynamic client to mange worker Kubernetes resources.
+type DynamicClient interface {
+	PatchResource(ctx context.Context, name, namespace string, gvr schema.GroupVersionResource, data []byte) (*unstructured.Unstructured, error)
+	DeleteResource(ctx context.Context, name, namespace string, gvr schema.GroupVersionResource) error
+}
+
+// NewDefaultDynamicClient creates a new DynamicClient.
+func NewDefaultDynamicClient(client dynamic.Interface) DynamicClient {
+	return &defaultDynamicClient{client: client}
+}
+
+type defaultDynamicClient struct {
+	client dynamic.Interface
+}
+
+// PatchResource patches a Kubernetes resources.
+func (c *defaultDynamicClient) PatchResource(ctx context.Context, name, namespace string, gvr schema.GroupVersionResource, data []byte) (*unstructured.Unstructured, error) {
+	dr := c.getResourceInterface(namespace, gvr)
+	return dr.Patch(ctx, name, types.ApplyPatchType, data, metav1.PatchOptions{FieldManager: fieldManager})
+}
+
+func (c *defaultDynamicClient) DeleteResource(ctx context.Context, name, namespace string, gvr schema.GroupVersionResource) error {
+	dr := c.getResourceInterface(namespace, gvr)
+	return dr.Delete(ctx, name, metav1.DeleteOptions{})
+}
+
+func (c *defaultDynamicClient) getResourceInterface(namespace string, gvr schema.GroupVersionResource) dynamic.ResourceInterface {
+	if namespace != "" {
+		return c.client.Resource(gvr).Namespace(namespace)
+	}
+	return c.client.Resource(gvr)
 }
