41157d9aae6d0ea73e07f64661a3db53bf1b8242
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-02-06 10:48:28 -0800

    feat: expose ListClusters and add more fields

--- dist/api/v1/job_manager_server.pb.d.ts
+++ dist/api/v1/job_manager_server.pb.d.ts
@@ -1,8 +1,15 @@
 import * as fm from "../../fetch.pb";
 import * as LlmarinerJobsServerV1Job_manager_server_worker from "./job_manager_server_worker.pb";
+export type ClusterSummary = {
+    gpuCapacity?: number;
+    gpuUsed?: number;
+    jobCount?: number;
+};
 export type Cluster = {
     id?: string;
+    name?: string;
     status?: LlmarinerJobsServerV1Job_manager_server_worker.ClusterStatus;
+    summary?: ClusterSummary;
     lastUpdatedAt?: string;
 };
 export type ListClustersRequest = {};
