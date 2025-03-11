163c001efc8165c1f5bd2ab0e6011d700321250f
Takeshi Yoshino <81723316+takeshi-cloudnatix@users.noreply.github.com>
2025-03-11 10:55:45 +0000

    fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 export * from "./api/v1/fine_tuning_service.pb";
 export * from "./api/v1/job_manager_server.pb";
+export * from "./api/v1/job_manager_server_worker.pb";
 export * from "./api/v1/workspace_service.pb";
 export * from "./fetch.pb";
