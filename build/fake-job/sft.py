0c1f9619b47f513cb7263435128ef95efa5ed309
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-09-19 12:32:35 -0700

    feat: get an error message from pod termination status (#495)

--- build/fake-job/sft.py
+++ build/fake-job/sft.py
@@ -1,6 +1,5 @@
 import os
 
-# Move files in the current directory to the "output" directory.
-files = os.listdir("./ft-output")
-for fn in files:
-    os.rename("./ft-output/" + fn, f"output/{fn}")
+print('hello')
+
+exit(1)
