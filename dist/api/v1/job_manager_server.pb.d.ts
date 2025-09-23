7a478772dafd966de034314ee90a47bb0e5b70c2
Guangrui Fu <guangrui@cloudnatix.com>
2025-09-23 00:20:53 +0000

    feat(proto): add ListJobSummaries message

--- dist/api/v1/job_manager_server.pb.d.ts
+++ dist/api/v1/job_manager_server.pb.d.ts
@@ -1,5 +1,11 @@
 import * as fm from "../../fetch.pb";
 import * as LlmarinerJobsServerV1Job_manager_server_worker from "./job_manager_server_worker.pb";
+export declare enum JobType {
+    JOB_TYPE_UNSPECIFIED = "JOB_TYPE_UNSPECIFIED",
+    JOB_TYPE_BATCH = "JOB_TYPE_BATCH",
+    JOB_TYPE_FINE_TUNING = "JOB_TYPE_FINE_TUNING",
+    JOB_TYPE_NOTEBOOK = "JOB_TYPE_NOTEBOOK"
+}
 export type ClusterSummary = {
     gpu_capacity?: number;
     gpu_allocated?: number;
@@ -16,6 +22,30 @@ export type ListClustersRequest = {};
 export type ListClustersResponse = {
     clusters?: Cluster[];
 };
+export type RequestFilter = {
+    start_timestamp?: string;
+    end_timestamp?: string;
+};
+export type ListJobSummariesRequest = {
+    filter?: RequestFilter;
+};
+export type ListJobSummariesResponseValue = {
+    job_type?: JobType;
+    total_created?: string;
+    total_completed?: string;
+    total_cancelled?: string;
+    total_failed?: string;
+    total_deleted?: string;
+    total_running?: string;
+};
+export type ListJobSummariesResponseDatapoint = {
+    timestamp?: string;
+    values?: ListJobSummariesResponseValue[];
+};
+export type ListJobSummariesResponse = {
+    datapoints?: ListJobSummariesResponseDatapoint[];
+};
 export declare class JobService {
     static ListClusters(req: ListClustersRequest, initReq?: fm.InitReq): Promise<ListClustersResponse>;
+    static ListJobSummaries(req: ListJobSummariesRequest, initReq?: fm.InitReq): Promise<ListJobSummariesResponse>;
 }
--- dist/api/v1/job_manager_server.pb.js
+++ dist/api/v1/job_manager_server.pb.js
@@ -4,8 +4,18 @@
 * This file is a generated Typescript file for GRPC Gateway, DO NOT MODIFY
 */
 import * as fm from "../../fetch.pb";
+export var JobType;
+(function (JobType) {
+    JobType["JOB_TYPE_UNSPECIFIED"] = "JOB_TYPE_UNSPECIFIED";
+    JobType["JOB_TYPE_BATCH"] = "JOB_TYPE_BATCH";
+    JobType["JOB_TYPE_FINE_TUNING"] = "JOB_TYPE_FINE_TUNING";
+    JobType["JOB_TYPE_NOTEBOOK"] = "JOB_TYPE_NOTEBOOK";
+})(JobType || (JobType = {}));
 export class JobService {
     static ListClusters(req, initReq) {
         return fm.fetchReq(`/v1/jobs/clusters?${fm.renderURLSearchParams(req, [])}`, Object.assign(Object.assign({}, initReq), { method: "GET" }));
+    }
+    static ListJobSummaries(req, initReq) {
+        return fm.fetchReq(`/v1/jobs/summaries?${fm.renderURLSearchParams(req, [])}`, Object.assign(Object.assign({}, initReq), { method: "GET" }));
     }
 }
